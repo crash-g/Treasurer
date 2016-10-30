479f9ce3641f2e43c67e0645103b9dde3766b015
crash <crash@inventati.org>
2016-10-30 13:26:06 +0100

    Added README

--- com/muciaccia/plugins/Treasurer.java
+++ com/muciaccia/plugins/Treasurer.java
@@ -16,16 +16,6 @@ import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-/**
- * This class is an implementation of a treasurer plugin for a generic bot. The plugin registers the expenses of a group
- * of users and provides an optimized way of giving money back with the minimum possible number of transactions.
- *
- * Allowed messages include: BALANCE (shows a summary of debts), HISTORY (shows a summary of user debts and credits),
- * messages to add expenses and messages to manage groups of users.
- *
- * In particular, messages to add expenses should have the form <AMOUNT>|<HANDLE>[,<HANDLE>]*[ "<MESSAGE>"], where HANDLE
- * is a user name or a group name with optional modifiers (+ and *) to allow uneven splitting.
- */
 public class Treasurer implements Plugin {
     // global settings for numbers
     public static final int NORMAL_SCALE = 2;
